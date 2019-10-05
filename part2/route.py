c245e8ecf4f4b719e71033c89d71729f9f8e5da6
Agrawal, Bhumika <bagrawal@iu.edu>
2019-10-05 14:23:54 -0400

    Add files via upload

--- part2/route.py
+++ part2/route.py
@@ -1,3 +1,86 @@
 #!/usr/local/bin/python3
 
 # put your routing program here!
+class Vertex:
+    def __init__(self, vertex):
+        self.name = vertex
+        self.neighbors = []
+        
+    def add_neighbor(self, neighbor):
+        if isinstance(neighbor, Vertex):
+            if neighbor.name not in self.neighbors:
+                self.neighbors.append(neighbor.name)
+                neighbor.neighbors.append(self.name)
+                self.neighbors = sorted(self.neighbors)
+                neighbor.neighbors = sorted(neighbor.neighbors)
+        else:
+            return False
+        
+    def add_neighbors(self, neighbors):
+        for neighbor in neighbors:
+            if isinstance(neighbor, Vertex):
+                if neighbor.name not in self.neighbors:
+                    self.neighbors.append(neighbor.name)
+                    neighbor.neighbors.append(self.name)
+                    self.neighbors = sorted(self.neighbors)
+                    neighbor.neighbors = sorted(neighbor.neighbors)
+            else:
+                return False
+        
+    def __repr__(self):
+        return str(self.neighbors)
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex):
+            self.vertices[vertex.name] = vertex.neighbors
+
+            
+    def add_vertices(self, vertices):
+        for vertex in vertices:
+            if isinstance(vertex, Vertex):
+                self.vertices[vertex.name] = vertex.neighbors
+            
+    def add_edge(self, vertex_from, vertex_to):
+        if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
+            vertex_from.add_neighbor(vertex_to)
+            if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
+                self.vertices[vertex_from.name] = vertex_from.neighbors
+                self.vertices[vertex_to.name] = vertex_to.neighbors
+                
+    def add_edges(self, edges):
+        for edge in edges:
+            self.add_edge(edge[0],edge[1])          
+    
+    def adjacencyList(self):
+        if len(self.vertices) >= 1:
+                return [str(key) + ":" + str(self.vertices[key]) for key in self.vertices.keys()]  
+        else:
+            return dict()
+
+def graph(g):
+    """ Function to print a graph as adjacency list and adjacency matrix. """
+    return str(g.adjacencyList()) 
+
+if __name__ == "__main__":
+    count = len(open("city-gps.txt",'r').readlines())
+    graph1 = Graph()
+    cities = []
+    i = 0
+    with open("city-gps.txt",'r') as file:
+        for line in file:
+            city_name = str.split(line)[0]
+            print(city_name)
+            cities.append(Vertex(city_name)) 
+            i += 1
+            print("Vertices are: ")
+            print(cities)
+            #graph1.add_vertex(str.split(line)[0])
+    
+
+    
+    print("Adjancency list is:")
+    print(graph(graph1))
