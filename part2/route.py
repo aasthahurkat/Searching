2b59508eac8ac29498fe216a01f37a694ceae010
Agrawal, Bhumika <bagrawal@iu.edu>
2019-10-05 16:21:02 -0400

    Add files via upload

--- part2/route.py
+++ part2/route.py
@@ -2,30 +2,20 @@
 
 # put your routing program here!
 class Vertex:
-    def __init__(self, vertex):
+    def __init__(self, vertex,lat,lon):
         self.name = vertex
         self.neighbors = []
+        self.lat = lat
+        self.lon = lon
         
-    def add_neighbor(self, neighbor):
+    def add_neighbor(self, neighbor,dist,limit,highway):
         if isinstance(neighbor, Vertex):
             if neighbor.name not in self.neighbors:
-                self.neighbors.append(neighbor.name)
-                neighbor.neighbors.append(self.name)
-                self.neighbors = sorted(self.neighbors)
-                neighbor.neighbors = sorted(neighbor.neighbors)
+                self.neighbors.append((neighbor.name,dist,limit,highway))
+                neighbor.neighbors.append((self.name,dist,limit,highway))
         else:
+            print("not added neighbour")
             return False
-        
-    def add_neighbors(self, neighbors):
-        for neighbor in neighbors:
-            if isinstance(neighbor, Vertex):
-                if neighbor.name not in self.neighbors:
-                    self.neighbors.append(neighbor.name)
-                    neighbor.neighbors.append(self.name)
-                    self.neighbors = sorted(self.neighbors)
-                    neighbor.neighbors = sorted(neighbor.neighbors)
-            else:
-                return False
         
     def __repr__(self):
         return str(self.neighbors)
@@ -36,25 +26,36 @@ class Graph:
     
     def add_vertex(self, vertex):
         if isinstance(vertex, Vertex):
+            #self.vertices[vertex.name] = vertex.neighbors
             self.vertices[vertex.name] = vertex.neighbors
 
-            
-    def add_vertices(self, vertices):
-        for vertex in vertices:
-            if isinstance(vertex, Vertex):
-                self.vertices[vertex.name] = vertex.neighbors
-            
-    def add_edge(self, vertex_from, vertex_to):
+    def add_edge(self, vertex_from, vertex_to,dist,limit,highway):
         if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
-            vertex_from.add_neighbor(vertex_to)
+            print("inside first if")
+            vertex_from.add_neighbor(vertex_to,dist,limit,highway)
             if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
+                print("inside second if")
                 self.vertices[vertex_from.name] = vertex_from.neighbors
                 self.vertices[vertex_to.name] = vertex_to.neighbors
-                
-    def add_edges(self, edges):
-        for edge in edges:
-            self.add_edge(edge[0],edge[1])          
-    
+            else:
+                print("inner else")
+        else:
+            print("add edge encountered else")
+     
+
+    def add_edge1(self, vertex_from, vertex_to,dist,limit,highway):
+        if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
+            print("inside first if")
+            vertex_from.add_neighbor(vertex_to,dist,limit,highway)
+            if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
+                print("inside second if")
+                self.vertices[vertex_from.name] = vertex_from.neighbors
+                self.vertices[vertex_to.name] = vertex_to.neighbors
+            else:
+                print("inner else")
+        else:
+            print("add edge encountered else")
+                            
     def adjacencyList(self):
         if len(self.vertices) >= 1:
                 return [str(key) + ":" + str(self.vertices[key]) for key in self.vertices.keys()]  
@@ -66,21 +67,29 @@ def graph(g):
     return str(g.adjacencyList()) 
 
 if __name__ == "__main__":
-    count = len(open("city-gps.txt",'r').readlines())
-    graph1 = Graph()
+    #count = len(open("city-gps.txt",'r').readlines())
+    US_map = Graph()
     cities = []
     i = 0
     with open("city-gps.txt",'r') as file:
         for line in file:
             city_name = str.split(line)[0]
-            print(city_name)
-            cities.append(Vertex(city_name)) 
-            i += 1
-            print("Vertices are: ")
-            print(cities)
-            #graph1.add_vertex(str.split(line)[0])
+            latitude = str.split(line)[1]
+            longitude = str.split(line)[2] 
+            #print("city name: ",city_name)
+            city = Vertex(city_name,latitude,longitude)
+            US_map.add_vertex(city)
     
-
+    with open("road-segments.txt",'r') as file:
+        for line in file:
+            from_city = str.split(line)[0]
+            print("from_city: ", from_city)
+            to_city = str.split(line)[1]
+            print("to-city: ", to_city)
+            length = str.split(line)[2]
+            speed_limit = str.split(line)[3]
+            highway_name = str.split(line)[4]
+            US_map.add_edge(from_city,to_city,length,speed_limit,highway_name)
     
     print("Adjancency list is:")
-    print(graph(graph1))
+    print(graph(US_map))
