157dcee8032e0eb7fe924e83012f21fee4a9de97
Agrawal, Bhumika <bagrawal@iu.edu>
2019-10-05 21:59:29 -0400

    Add files via upload

--- part2/route_v3.py
+++ part2/route_v3.py
@@ -1,6 +1,6 @@
 adj_list = {}
-def loadFile(adj_list):
-    with open("C:\\Users\\Rohit\\Documents\\AI\\commit\\bagrawal-aahurkat-rrokde-a1\\part2\\road-segments.txt",'r') as file:
+def createAdjList(filename):
+    with open(filename,'r') as file:
         for line in file:
             from_city = str.split(line)[0]
             to_city = str.split(line)[1]
@@ -19,6 +19,9 @@ def loadFile(adj_list):
                 old = adj_list[to_city]
                 old.append((from_city,length,speed_limit,highway_name))
                 adj_list[to_city] = old
+            else:
+                adj_list[to_city] = []
+                adj_list[to_city].append((from_city,length,speed_limit,highway_name))
  
 def successors(args):
     possible_list = args[0]
@@ -36,7 +39,7 @@ def solve(source, destination):
     while len(fringe) > 0:
         elem = fringe.pop()
         for neighbour in successors(elem):
-            travelle_cost = 
+            travelle_cost = 1
 
 
     return True
@@ -53,4 +56,3 @@ if __name__ == "__main__":
     for key in adj_list:
         print("no of neighbours: ", len(adj_list[key]), " for city: ", key)
         print(key, '->', adj_list[key])
-
